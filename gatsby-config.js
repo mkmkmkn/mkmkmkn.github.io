8dc4b0604368a49381fd80346b2e3803e34f8aff
mkmkmkn <mkgoma3@gmail.com>
2026-08-05 13:59:30 +0900

    node-sass廃止

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,11 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    'sass',
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("sass"),
+      },
+    }
   ],
 }
